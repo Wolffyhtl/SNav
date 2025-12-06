1077ac29c827bf25fb7d1b4206cd70920d5587e9
Wolffyhtl <matextsanzhedie@outlook.com>
2025-12-06 22:26:54 +0800

    Update GitHub link in Footer component

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -25,7 +25,7 @@
         </div>
         <div class="desc">
           <n-space class="link" justify="center">
-            <n-button strong secondary @click="jumpTo('https://github.com/imsyy/Snavigation')">
+            <n-button strong secondary @click="jumpTo('https://github.com/Wolffyhtl')">
               Github
             </n-button>
           </n-space>
